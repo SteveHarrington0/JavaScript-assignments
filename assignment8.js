156bf9bf95d3c1dfa361e035164351049b9cfe62
Gopi Vanama <107838189+SteveHarrington0@users.noreply.github.com>
2022-11-04 14:27:28 +0530

    Add files via upload

---
+++ assignment8.js
@@ -0,0 +1,39 @@
+// 1.we can't put duplicate values in set object
+
+
+
+
+// 2.
+    //a
+
+
+    // let set = new Set();
+
+
+    // //b
+
+
+    // set.add(4);
+    // set.add(5);
+    // set.add(6);
+
+
+    // //c
+
+
+    // set.delete(5);
+    // console.log(set)
+
+
+// 3.
+
+// let set = new Set();
+// for (let i = 1; i <= 4; i++){
+//     set.add(Math.floor(Math.random() * 11))
+//     if (i != set.size){
+//         i--;
+//     }
+// }
+// let bool = set.has(8);
+// console.log(set, bool)
+
