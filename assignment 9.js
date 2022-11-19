a8f5b17984b210ad99c8c1b6a9241e4d91dd93b3
Gopi Vanama <107838189+SteveHarrington0@users.noreply.github.com>
2022-11-19 21:35:30 +0530

    Add files via upload

---
+++ assignment 9.js
@@ -0,0 +1,20 @@
+// 1.a yes inner function is a closure .
+
+//  1.b   It will alert 100 in the browser
+
+
+// 2. closure is a function having variable reference which is outside the 
+//    function that is running which we called as lexical scope.
+//    and scope can be limited to certain function or loop or entire 
+//    program depending on the intialization.
+
+
+// 3. lexical scope is initializing the variable outside the function 
+//    and using it in the function without intializing the variable inside
+//    the function.
+//    closure is the function which is using the variable having lexical scope
+
+
+// 4. 0
+//    1
+//    2  
