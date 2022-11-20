9bdbc3ee5e86e7f03c88b88c64b11bd1a3ee9d57
Gopi Vanama <107838189+SteveHarrington0@users.noreply.github.com>
2022-11-20 10:25:35 +0530

    Add files via upload

---
+++ assignment10.js
@@ -0,0 +1,35 @@
+// 1. Higher order functions and callback functions are different
+//    Higher order functions take callback functions as parameters
+//    and returns the function as a value
+//    callback functions are used inside the higher order functions
+
+
+
+
+
+// 2. Filter is a higher order function cause it takes
+//    a function as a parameter which return true or false according to the condition.
+
+
+
+// 3. Example :
+
+/*function callback(a,b){
+    return a+b;
+}
+function higherorderfunction(callback){
+    let c = 50;
+    return (callback(30,20) + c);
+}
+let result = higherorderfunction(callback);
+console.log(result);
+*/
+
+
+// 4. 
+// a. Hello john 
+//    Hello tina
+//    Hello kale
+//    Hello max
+
+// b. useFunction is the higher order function
